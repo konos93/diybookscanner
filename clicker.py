d5e334d3b39eb8052d772e79620ff02aa43b8a98
konos93 <konos_mk93@hotmail.com>
2024-02-21 06:15:47 +0200

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 
 from libcamera import controls
-from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5 import QtWidgets
-
+from PyQt5 import QtCore, QtGui, QtWidgets
 from picamera2 import Picamera2
 from picamera2.previews.qt import QGlPicamera2
-
 import time
 import pyautogui
+import RPi.GPIO as GPIO
 
-
+# GPIO pin number for the trigger
+GPIO_TRIGGER_PIN = 18
 
 def automate_task():
     tool1_position = (1800, 400)
-    
     tool2_position = (1800, 900)
-
-    pyautogui.click(tool1_position,clicks=2, interval=0.01)
-
-    pyautogui.click(tool2_position,clicks=2, interval=0.01)
+    pyautogui.click(tool1_position, clicks=2, interval=0.01)
+    pyautogui.click(tool2_position, clicks=2, interval=0.01)
     print("Automation completed.")
 
 def keyPressEvent(event):
@@ -53,7 +48,21 @@ def create_gui():
 def click_both():
     print("Click Both button clicked!")
     automate_task()
-    pyautogui.moveTo(200,400)# if cursor is not moved button seems to be pushed 
-    
+    pyautogui.moveTo(200, 400)  # if cursor is not moved button seems to be pushed 
+
+def gpio_callback(channel):
+    print("GPIO 18 triggered!")
+    automate_task()
+
 if __name__ == "__main__":
+    # Set up GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_TRIGGER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(GPIO_TRIGGER_PIN, GPIO.FALLING, callback=gpio_callback, bouncetime=300)
+
     create_gui()
+
+    # Clean up GPIO
+    GPIO.remove_event_detect(GPIO_TRIGGER_PIN)
+    GPIO.cleanup()
+
